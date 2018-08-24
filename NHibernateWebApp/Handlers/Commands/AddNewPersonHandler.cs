3e403f2a352e9b40d9f0b05e10ba558820cc5695
marc.harry <marc.harry@dezrez.com>
2018-08-24 15:00:40 +0100

    Added MediatR and some queries

---
+++ NHibernateWebApp/Handlers/Commands/AddNewPersonHandler.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using NHibernateWebApp.Database;
+using NHibernateWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NHibernateWebApp.Handlers.Commands
+{
+    public class AddNewPersonHandler : AsyncRequestHandler<AddNewPerson>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AddNewPersonHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        protected override Task Handle(AddNewPerson request, CancellationToken cancellationToken)
+        {
+            var person = new Person { Name = request.Name };
+
+            if (!string.IsNullOrWhiteSpace(request.AddressLine1)
+                && !string.IsNullOrWhiteSpace(request.Postcode))
+            {
+                person.Address = new Address
+                {
+                    AddressLineOne = request.AddressLine1,
+                    AddressLineTwo = request.AddressLine2,
+                    City = request.City,
+                    Postcode = request.Postcode
+                };
+            }
+
+            _unitOfWork.Session.Save(person);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ NHibernateWebApp/Handlers/Queries/GetPersonById.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using NHibernateWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NHibernateWebApp.Handlers.Queries
+{
+    public class GetPersonById : IRequest<Person>
+    {
+        public long Id { get; set; }
+    }
+}
